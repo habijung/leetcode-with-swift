5153e6cdca1bca0335e31d958dc93db4c28cd043
habijung <habijung0@gmail.com>
2025-07-16 13:00:54 +0900

    Add new Swift Testing target

---
+++ Card/Arrays 101/MaxConsecutiveOnes.swift
@@ -0,0 +1,8 @@
+//
+//  MaxConsecutiveOnes.swift
+//  RunCode
+//
+//  Created by hbjung on 7/16/25.
+//
+
+import Foundation
---
+++ RunCode/RunCode.swift
@@ -0,0 +1,16 @@
+//
+//  RunCode.swift
+//  RunCode
+//
+//  Created by hbjung on 7/16/25.
+//
+
+import Testing
+
+struct RunCode {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
