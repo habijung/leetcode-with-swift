dce0b51571144ac746dfb3109e1051896f8bae02
habijung <habijung0@gmail.com>
2025-07-19 20:28:19 +0900

    Solved: [Arrays 101] 1089. Duplicate Zeros (2)

--- Card/Arrays 101/1089.DuplicateZeros.swift
+++ Card/Arrays 101/1089.DuplicateZeros.swift
@@ -34,15 +34,48 @@ class Solution1089 {
         
         arr = Array(arrCopy[..<arr.count])
     }
+    
+    func duplicateZeros2(_ arr: inout [Int]) {
+        var zerosCount = 0
+        
+        // 전체 0의 개수 확인
+        for num in arr {
+            if num == 0 {
+                zerosCount += 1
+            }
+        }
+        
+        // arr를 뒤에서부터 읽으면서 값이 0을 확장한 arr 범위에 있으면 추가
+        var readIndex = arr.count - 1
+        var writeIndex = arr.count - 1 + zerosCount
+        
+        while readIndex >= 0 {
+            if writeIndex < arr.count {
+                arr[writeIndex] = arr[readIndex]
+            }
+                
+            if arr[readIndex] == 0 {
+                writeIndex -= 1
+                
+                if writeIndex < arr.count {
+                    arr[writeIndex] = arr[readIndex]
+                }
+            }
+            
+            readIndex -= 1
+            writeIndex -= 1
+        }
+    }
 }
 
 struct Test1089 {
     let solution = Solution1089()
-
+    
     static let testCases: [(input: [Int], expected: [Int])] = [
         ([1, 0, 2, 3, 0, 4, 5, 0], [1, 0, 0, 2, 3, 0, 0, 4]),
         ([1, 2, 3], [1, 2, 3]),
         ([1, 0], [1, 0]),
+        ([0], [0]),
     ]
     
     @Test("1089. Duplicate Zeros", arguments: testCases)
@@ -50,6 +83,15 @@ struct Test1089 {
         var arr = input
         
         solution.duplicateZeros(&arr)
+        
+        #expect(arr == expected)
+    }
+    
+    @Test("1089. Duplicate Zeros 2", arguments: testCases)
+    func runSolution2(input: [Int], expected: [Int]) async throws {
+        var arr = input
+        
+        solution.duplicateZeros2(&arr)
         
         #expect(arr == expected)
     }
