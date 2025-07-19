19604e54265dde69ca8267c0bdbfa19a86576ffe
habijung <habijung0@gmail.com>
2025-07-20 00:10:20 +0900

    Update template file for function inout parameter

--- Template/Template.swift
+++ Template/Template.swift
@@ -9,14 +9,19 @@ import Foundation
 import Testing
 
 class SolutionNum {
-    func funcName(_ num: Int) -> Int {
+    func funcDefault(_ num: Int) -> Int {
         return num
+    }
+
+    func funcInout(_ nums: inout [Int]) {
+        // Write your code.
     }
 }
 
 struct TestNum {
     let solution = SolutionNum()
 
+    // Test for default function
     static let testCases: [(input: Int, expected: Int)] = [
         (0, 0),
         (1, 1),
@@ -25,6 +30,19 @@ struct TestNum {
     @Test("Num. Title", arguments: testCases)
     func runSolution(input: Int, expected: Int) async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        #expect(solution.funcName(input) == expected)
+        #expect(solution.funcDefault(input) == expected)
+    }
+
+    // Test for inout function
+    static let testCasesInout: [(input: [Int], expected: [Int])] = [
+        ([0, 0], [0, 0]),
+        ([1, 2, 3], [1, 2, 3]),
+    ]
+
+    @Test("Num. Title for inout", arguments: testCasesInout)
+    func runSolutionInout(input: [Int], expected: [Int]) async throws {
+        var input = input
+        solution.funcInout(&input)
+        #expect(input == expected)
     }
 }
