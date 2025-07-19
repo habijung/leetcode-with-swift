1b53710574569798091232f8aea8a63869ef4d80
habijung <habijung0@gmail.com>
2025-07-19 21:57:09 +0900

    Reformat code with SwiftFormat

--- Card/Arrays 101/1295.FindNumbersWithEvenNumberOfDigits.swift
+++ Card/Arrays 101/1295.FindNumbersWithEvenNumberOfDigits.swift
@@ -8,30 +8,28 @@
 import Foundation
 import Testing
 
-
 class Solution1295 {
     func findNumbers(_ nums: [Int]) -> Int {
-        var count: Int = 0
-        
+        var count = 0
+
         for num in nums {
             if String(num).count.isMultiple(of: 2) {
                 count += 1
             }
         }
-        
+
         return count
     }
 }
 
-
 struct Test1295 {
     let solution = Solution1295()
-    
+
     static let testCases: [(input: [Int], expected: Int)] = [
         ([12, 345, 2, 6, 7896], 2),
         ([555, 901, 482, 1771], 1),
     ]
-    
+
     @Test("1295. Find Numbers with Even Number of Digits", arguments: testCases)
     func runSolution(input: [Int], expected: Int) async throws {
         #expect(solution.findNumbers(input) == expected)
